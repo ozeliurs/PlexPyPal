856c5b6854814fac1742bbe3cb68f9819bfa50cb
Ozeliurs <ozeliurs@gmail.com>
2023-01-26 15:21:23 +0100

    feat: lots of things

---
+++ discordWebhook/webhooks.py
@@ -0,0 +1,122 @@
+from discordWebhook.discordWebhookEmbed import DiscordWebhookEmbed
+from discordWebhook.discordEmbedColor import DiscordEmbedColor
+
+
+class BaseEmbed(DiscordWebhookEmbed):
+    def __init__(self, msg):
+        super().__init__()
+        self.set_username("PlexPyPal")
+        self.set_timestamp()
+
+        if msg.get("Metadata").get("grandparentTitle"):
+            title = msg.get("Metadata").get("grandparentTitle")
+
+            if msg.get("Metadata").get("index") and msg.get("Metadata").get("parentIndex"):
+                title += f" S{msg.get('Metadata').get('parentIndex')}E{msg.get('Metadata').get('index')}"
+
+        else:
+            title = msg.get("Metadata").get("title")
+
+        if msg.get("Metadata").get("year"):
+            title += f" ({msg.get('Metadata').get('year')})"
+
+        self.set_title(title)
+
+
+class LibraryOnDeck(BaseEmbed):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.set_author("New media on deck")
+        self.set_description(f"{msg.get('Account').get('title')}")
+        self.set_color(DiscordEmbedColor.BLUE)
+
+
+class LibraryNew(BaseEmbed):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.set_author("New media added")
+        self.set_description(f"{msg.get('Account').get('title')}")
+        self.set_color(DiscordEmbedColor.BLUE)
+
+
+class MediaPause(BaseEmbed):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.set_author("Media paused")
+        self.set_description(f"{msg.get('Account').get('title')}")
+        self.set_color(DiscordEmbedColor.YELLOW)
+
+
+class MediaPlay(BaseEmbed):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.set_author("Media playing")
+        self.set_description(f"{msg.get('Account').get('title')}")
+        self.set_color(DiscordEmbedColor.GREEN)
+
+
+class MediaRate(BaseEmbed):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.set_author("Media rated")
+        self.set_description(f"{msg.get('Account').get('title')}")
+        self.set_color(DiscordEmbedColor.WHITE)
+
+
+class MediaResume(BaseEmbed):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.set_author("Media resumed")
+        self.set_description(f"{msg.get('Account').get('title')}")
+        self.set_color(DiscordEmbedColor.GREEN)
+
+
+class MediaScrobble(BaseEmbed):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.set_author("Media scrobbled")
+        self.set_description(f"{msg.get('Account').get('title')}")
+        self.set_color(DiscordEmbedColor.WHITE)
+
+
+class MediaStop(BaseEmbed):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.set_author("Media stopped")
+        self.set_description(f"{msg.get('Account').get('title')}")
+        self.set_color(DiscordEmbedColor.RED)
+
+
+class AdminDatabaseBackup(BaseEmbed):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.set_author("Database backup")
+        self.set_color(DiscordEmbedColor.GREEN)
+
+
+class AdminDatabaseCorruption(BaseEmbed):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.set_author("Database corruption")
+        self.set_color(DiscordEmbedColor.RED)
+
+
+class DeviceNew(BaseEmbed):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.set_author("New device")
+        self.set_color(DiscordEmbedColor.YELLOW)
+
+
+class PlaybackStarted(BaseEmbed):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.set_author("Playback started")
+        self.set_color(DiscordEmbedColor.GREEN)
+
+
+class UnknownEvent(BaseEmbed):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.set_author("Unknown event")
+        self.set_color(DiscordEmbedColor.MAGENTA)
--- plexParser.py
+++ plexParser.py
@@ -1,127 +1,30 @@
-from discordWebhook.discordWebhookEmbed import DiscordWebhookEmbed
-from discordWebhook.discordEmbedColor import DiscordEmbedColor
-
-
-def base_embed(msg: dict):
-    embed = DiscordWebhookEmbed()
-    embed.set_username("PlexPyPal")
-    embed.set_description(msg.get("Metadata").get("title"))
-    # embed.set_thumbnail(msg.get("Metadata").get("thumb"))
-    embed.set_timestamp()
-    return embed
-
-
-def library_on_deck(msg: dict):
-    embed = base_embed(msg)
-    embed.set_author("New media on deck")
-    return embed
-
-
-def library_new(msg: dict):
-    embed = base_embed(msg)
-    embed.set_author("New media added")
-    return embed
-
-
-def media_pause(msg: dict):
-    embed = base_embed(msg)
-    embed.set_author("Media paused")
-    embed.set_title(msg.get('Metadata').get('title'))
-    return embed
-
-
-def media_play(msg: dict):
-    embed = base_embed(msg)
-    embed.set_author("Media playing")
-    embed.set_title(msg.get('Metadata').get('title'))
-    embed.set_description(f"{msg.get('Account').get('title')} has started playing {msg.get('Metadata').get('title')}")
-    embed.set_color(DiscordEmbedColor.GREEN)
-    return embed
-
-
-def media_rate(msg: dict):
-    embed = base_embed(msg)
-    embed.set_author("Media rated")
-    embed.set_title(msg.get('Metadata').get('title'))
-    return embed
-
-
-def media_resume(msg: dict):
-    embed = base_embed(msg)
-    embed.set_author("Media resumed")
-    embed.set_title(msg.get('Metadata').get('title'))
-    return embed
-
-
-def media_scrobble(msg: dict):
-    embed = base_embed(msg)
-    embed.set_author("Media scrobbled")
-    embed.set_title(msg.get('Metadata').get('title'))
-    return embed
-
-
-def media_stop(msg: dict):
-    embed = base_embed(msg)
-    embed.set_author("Media stopped")
-    return embed
-
-
-
-def admin_database_backup(msg: dict):
-    embed = base_embed(msg)
-    embed.set_author("Database backup")
-    return embed
-
-
-def admin_database_corruption(msg: dict):
-    embed = base_embed(msg)
-    embed.set_author("Database corruption")
-    return embed
-
-
-def device_new(msg: dict):
-    embed = base_embed(msg)
-    embed.set_author("New device")
-    return embed
-
-
-def playback_started(msg: dict):
-    embed = base_embed(msg)
-    embed.set_author("Playback started")
-    return embed
-
-
-def unknown_event(msg: dict):
-    embed = base_embed(msg)
-    embed.set_author("Unknown event")
-    embed.set_description(msg.get("event"))
-    return embed
+from discordWebhook.webhooks import LibraryOnDeck, LibraryNew, MediaPause, MediaPlay, MediaRate, MediaResume, MediaScrobble, MediaStop, AdminDatabaseBackup, AdminDatabaseCorruption, DeviceNew, PlaybackStarted, UnknownEvent
 
 
 def parse_plex_json(msg: dict):
     if msg.get("event") == "library.on.deck":
-        return library_on_deck(msg)
+        return LibraryOnDeck(msg)
     elif msg.get("event") == "library.new":
-        return library_new(msg)
+        return LibraryNew(msg)
     elif msg.get("event") == "media.pause":
-        return media_pause(msg)
+        return MediaPause(msg)
     elif msg.get("event") == "media.play":
-        return media_play(msg)
+        return MediaPlay(msg)
     elif msg.get("event") == "media.rate":
-        return media_rate(msg)
+        return MediaRate(msg)
     elif msg.get("event") == "media.resume":
-        return media_resume(msg)
+        return MediaResume(msg)
     elif msg.get("event") == "media.scrobble":
-        return media_scrobble(msg)
+        return MediaScrobble(msg)
     elif msg.get("event") == "media.stop":
-        return media_stop(msg)
+        return MediaStop(msg)
     elif msg.get("event") == "admin.database.backup":
-        return admin_database_backup(msg)
+        return AdminDatabaseBackup(msg)
     elif msg.get("event") == "admin.database.corruption":
-        return admin_database_corruption(msg)
+        return AdminDatabaseCorruption(msg)
     elif msg.get("event") == "device.new":
-        return device_new(msg)
+        return DeviceNew(msg)
     elif msg.get("event") == "playback.started":
-        return playback_started(msg)
+        return PlaybackStarted(msg)
     else:
-        return unknown_event(msg)
+        return UnknownEvent(msg)
